715b7a3f0b23fcba6afdbb05bedca39906409845
Taman Hassan <tamanhassan1@gmail.com>
2025-10-16 14:12:55 +0200

    initial commit

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,10 @@
 let priceInput = prompt("Enter the price tag ($49.99):");
-if (priceWithoutDollar) = priceInput.replace("$", "")
 let priceWithoutDollar = priceInput.replace("$", "");
 let price = parseFloat(priceWithoutDollar);
-let discountedPrice = price * 0.9;
-console.log("Your new price is: $" + discountedPrice.toFixed(2));
+if(isNaN (price)) {
+    console.log("Invalid price entered. Please enter a valid number.");
+} else {
+    let discountedPrice = price * 0.9;
+    console.log("Your new price is: $" + discountedPrice.toFixed(2));
+} 
+
