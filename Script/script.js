6210aed8e8c223fc4e3ce66f7e09216523fc3bc6
Taman Hassan <tamanhassan1@gmail.com>
2025-10-16 13:59:51 +0200

    updated code script.js

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,6 @@
 let priceInput = prompt("Enter the price tag ($49.99):");
+if (priceWithoutDollar) = priceInput.replace("$", "")
 let priceWithoutDollar = priceInput.replace("$", "");
 let price = parseFloat(priceWithoutDollar);
-let duscountedPrice = price * 0.9;
+let discountedPrice = price * 0.9;
 console.log("Your new price is: $" + discountedPrice.toFixed(2));
